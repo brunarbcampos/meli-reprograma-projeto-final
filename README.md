becfeac7d518caeac53f363f545026fa7a78ed3a
brunarbcampos <brunarbcampos@gmail.com>
2019-12-16 14:18:04 -0300

    update final

--- README.md
+++ README.md
@@ -1,2 +1,13 @@
-# meli-reprograma-projeto-final
+# Pra Onde Viajar?
 Projeto final do bootcamp de Back-End da Reprograma (Turma N1-MELI)
+
+O projeto é uma API que gera destinos para pessoas que não sabem pra onde viajar. Essa API tem opções tais como: ver destinos pelo Estado onde estão localizados, escolher destinos por um clima específico ou até gerar um destino aleatório, caso o usuário esteja aberto à descobertas novas.
+
+## Linguagens
+* Node.js
+* Banco de dados
+* MongoDB
+
+## Ferramentas
+* Postman
+* Heroku
--- doc/api_project.js
+++ doc/api_project.js
@@ -2,14 +2,14 @@ define({
   "name": "Pra onde viajar",
   "version": "0.1.0",
   "description": "API do projeto final do Reprograma",
-  "title": "API final Reprograma",
+  "title": "API Pra Onde Viajar",
   "url": "https://github.com/brunarbcampos/meli-reprograma-projeto-final",
   "sampleUrl": false,
   "defaultVersion": "0.0.0",
   "apidoc": "0.3.0",
   "generator": {
     "name": "apidoc",
-    "time": "2019-12-14T17:54:37.855Z",
+    "time": "2019-12-16T15:40:50.126Z",
     "url": "http://apidocjs.com",
     "version": "0.19.0"
   }
